fd8842f87d7154e639578d518adc14ca2e6f0913
Jens Schumacher <js.@familie-schumacher.info>
2024-06-05 10:48:58 +0200

    changes build to gradle

---
+++ lib/src/test/kotlin/info/maila/lang/EncodingDetectorTest.kt
@@ -0,0 +1,19 @@
+package info.maila.lang
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class EncodingDetectorTest {
+
+    @Test
+    fun detectEncoding() {
+        assertTrue(true)
+    }
+
+    @Test
+    fun stringWithDetectedEncoding() {
+        assertTrue(true)
+    }
+
+}
